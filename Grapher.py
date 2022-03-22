da172f7f25a70febd1929277dcdbb2f9c01edbd3
Ohm <joshiohmka@gmail.com>
2022-03-22 23:08:52 +0530

    YelloColorChaBaba

--- Grapher.py
+++ Grapher.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-print("Welocme to OHM'S GRAPHER !")
+print("Welocme to OHM'S GRAPHER ! Enter an equation an the program will draw a graph.")
 x = np.linspace(-2,2,1000000)
 y = 2*x**2+10
 plt.plot(x,y, ':k')
