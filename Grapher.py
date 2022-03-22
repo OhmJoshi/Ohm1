121547b7f2018d773b6cf2154c55ff91d561e6e6
Ohm <joshiohmka@gmail.com>
2022-03-22 22:23:59 +0530

    Change1grapher

--- Grapher.py
+++ Grapher.py
@@ -5,7 +5,7 @@ print("Welocme to OHM'S GRAPHER !")
 x = np.linspace(-2,2,1000000)
 y = 2*x**2+10
 plt.plot(x,y, ':k')
-plt.title('Graph of your equation')
+plt.title('Graph of your equation..')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.grid()
